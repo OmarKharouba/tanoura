f0075c2b3e0b10d3ff09e65deb7bb5cc66f1bd90
Omar Kharouba <omar_kharouba@yahoo.com>
2019-01-06 15:59:19 +0200

    added tanouras crud routes

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,28 @@
+// Dependencies
 var express = require('express');
-var app = express();
-var Tanoura = require('./models/tanoura');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+// Database configuration
 var dbConfig = require('./config/database');
+
+// Routers
 var tanourasRouter = require('./routers/tanouras');
 
+// Initializing the app
+var app = express();
 
+// Body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Database path
 var db = dbConfig.database;
 
+// Avoid deprecated mongoose method
+mongoose.set('useFindAndModify', false);
+
+// Connecting to the database
 mongoose.connect(db, { useNewUrlParser: true });
 var connection = mongoose.connection;
 
@@ -19,20 +34,18 @@ connection.on('error', (err) => {
     console.log('error in database connection');
 });
 
+// Tanouras router
 app.use('/tanouras', tanourasRouter);
 
 
-
-
-
-
-app.all('*',(req,res)=>{
+// Handling requests to non-existing routes
+app.all('*', (req, res) => {
     res.status(404).json({
         message: "Page not found"
     });
 });
 
-
+// Starting the server
 var port = 3000;
 app.listen(port, () => {
     console.log('server is running on port ' + port);
--- back-end/routers/tanouras.js
+++ back-end/routers/tanouras.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var Tanoura = require('../models/tanoura');
 
-router.get('/all', (req, res) => {
+router.get('/', (req, res) => {
     Tanoura.find((err, tanouras) => {
         if (err) {
-            res.status(5000).json({
-                message: 'Error while retrieving tanouras'
+            res.status(500).json({
+                message: err.message
             });
         } else {
             res.status(200).json({
@@ -16,18 +16,69 @@ router.get('/all', (req, res) => {
     });
 });
 
-router.post('/new', (req, res) => {
+router.post('/', (req, res) => {
     var newTanoura = new Tanoura(req.body.tanoura);
     newTanoura.save((err, tanoura) => {
         if (err) {
-            res.status(5000).json({
-                message: 'Error while adding tanoura'
+            res.status(500).json({
+                message: err.message
             });
         } else {
             res.status(200).json({
                 tanoura: tanoura
             })
         }
+    });
+});
+
+router.put('/', (req, res) => {
+    var id = req.body.tanoura._id;
+    Tanoura.findOneAndUpdate({ _id: id }, req.body.tanoura, { new: true}, (err, tanoura) => {
+        if (err) {
+            res.status(500).json({
+                message: err.message
+            });
+        } else {
+            res.status(200).json({
+                tanoura: tanoura
+            })
+        }
+    });
+});
+
+router.delete('/', (req, res) => {
+    var id = req.body.id;
+    Tanoura.findOneAndDelete({ _id: id }, (err) => {
+        if (err) {
+            res.status(500).json({
+                message: err.message
+            });
+        } else {
+            res.status(200).json({
+                message: 'Tanoura is deleted successfully'
+            })
+        }
+    });
+});
+
+router.get('/:id', (req, res) => {
+    var id = req.params.id;
+    Tanoura.findById(id, (err, tanoura) => {
+        if (err) {
+            res.status(500).json({
+                message: err.message
+            });
+        } else {
+            res.status(200).json({
+                tanoura: tanoura
+            })
+        }
+    });
+});
+
+router.all('*', (req, res) => {
+    res.status(404).json({
+        message: "Page not found"
     });
 });
 
